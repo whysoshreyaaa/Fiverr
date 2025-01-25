68b36d62ba6cbe169165021f86aa9d80bac3931b
Shalini Srivastava <159019629+whysoshreyaaa@users.noreply.github.com>
2025-01-25 19:27:08 +0530

    Add files via upload

---
+++ Elastic Search/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import SearchApp from './components/SearchApp';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchApp />
+    </div>
+  );
+}
+
+export default App;
