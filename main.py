3528b79e8032b8b72beaddee5e642201e0e7cad4
Shalini Srivastava <159019629+whysoshreyaaa@users.noreply.github.com>
2025-01-26 18:07:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,8 @@ S3_BUCKET = "icc-cases"
 s3_client = boto3.client(
     's3',
     aws_access_key_id=AWS_ACCESS_KEY,
-    aws_secret_access_key=AWS_SECRET_KEY
+    aws_secret_access_key=AWS_SECRET_KEY,
+    region_name="ap-south-1"
 )    
 
 pdf_mappings = {}
