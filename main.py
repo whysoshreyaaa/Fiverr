361ff5e0035e9b58932a71cbcc8a7378a6643fad
Shalini Srivastava <159019629+whysoshreyaaa@users.noreply.github.com>
2025-01-27 19:10:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
     expose_headers=["*"],
+    allow_websockets=True
     max_age=3600,
 )
 
