4abcdda3adb8854afa7c209d544ba12fa53699a5
Shalini Srivastava <159019629+whysoshreyaaa@users.noreply.github.com>
2025-01-29 04:07:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,8 @@ class ElasticsearchClient_SSLConnection:
             http_auth=("elastic", "HmtoTvKY"),
             verify_certs=True,
             ca_certs="/app/certs/ca_certificate.pem",
+            ssl_certfile="/app/certs/client-certificate.pem",
+            ssl_keyfile="/app/certs/client-key.pem",
             compatibility_mode=False,
             headers={"Content-Type": "application/json"}# Update path if needed
         )
